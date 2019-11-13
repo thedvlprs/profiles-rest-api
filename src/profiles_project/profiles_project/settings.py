e7cca707a1855d89f628c6a8134bfb6bfeb72104
Fyunka <fyunkafinka@hotmail.com>
2019-11-13 22:54:29 +0500

    feat: change on DEBUG setting

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=2p^(fqyk+q=hb8e=a@!0y=wfkjoz#b)t=iii6k47ps%iim0iq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 
     '0.0.0.0',
